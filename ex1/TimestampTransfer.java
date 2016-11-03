38e5f7dc823b398323325389b15f399d5726d320
mengfanqi <1255312814@qq.com>
2016-11-03 14:04:49 +0800

    Update TimestampTransfer.java

--- ex1/TimestampTransfer.java
+++ ex1/TimestampTransfer.java
@@ -16,16 +16,27 @@ import java.util.Scanner;
 public class TimestampTransfer {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
+		//标准输入
 		Scanner scanner = new Scanner(System.in);
+		//更改日期格式（输入的格式）
 		SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		//更改日期格式（输出的日期）
 		SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		//用while循环从标准输入获取值
 		while (scanner.hasNext()){
+			//将获取的值放入line
 			String line = scanner.nextLine();
+			//定义一个Date类型的变量
 			Date lineDate = null;
+			//定义一个long类型的变量
 			long lineTimestamp;
+			//异常处理
 			try {
+				//将字符串类型转成输入格式的日期类型并赋给lineDate
 				lineDate = inputFormat.parse(line);
+				//将lineDate中的日期转换成long类型赋给lineTimestamp
 				lineTimestamp = lineDate.getTime();
+				//将lineDate转换成输出格式的日期类型并输出+to+lineTimestamp
 				System.out.println(outputFormat.format(lineDate) + " to " + lineTimestamp);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
